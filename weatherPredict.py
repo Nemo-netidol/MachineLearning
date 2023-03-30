03cb773b40b2f09ab76a598afe1aaac962dfad9c
Nemo Nakorn <75536335+Nemo-netidol@users.noreply.github.com>
2023-03-30 17:56:57 +0700

    weatherPredict update

--- weatherPredict.py
+++ weatherPredict.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 dataset = pd.read_csv("https://raw.githubusercontent.com/kongruksiamza/MachineLearning/master/Linear%20Regression/Weather.csv")
 
@@ -28,12 +29,21 @@ df = pd.DataFrame({'Actual_data':y_test.flatten(), 'Predicted_data':y_predict.fl
 
 df1 = df.head(20)
 
-df1.plot(kind='bar', figsize=(16, 10))
+# df1.plot(kind='bar', figsize=(16, 10))
 # plt.show()
 # figsize : a tuple of the width and height of the figure in inches
 
+# กราฟของข้อมูลทั้งหมด
 # dataset.plot(x='MinTemp', y='MaxTemp', style='o')
 # plt.title("Min & Max Temp")
 # plt.xlabel("MinTemp")
 # plt.ylabel("MaxTemp")
 plt.show()
+
+#Efficiency Measurement (Loss Function)
+print(f'MAE = {metrics.mean_absolute_error(y_test, y_predict)}')
+print(f'MSE = {metrics.mean_squared_error(y_test, y_predict)}')
+print(f'RMSE = {np.sqrt(metrics.mean_squared_error(y_test, y_predict))}')
+
+# R squared (a statistical measure of how well the regression line approximates the actual data.)
+print(f'R squared = {metrics.r2_score(y_test, y_predict) * 100:.2f}%')
