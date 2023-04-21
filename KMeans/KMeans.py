0bbb309557c490c0d7455d74c0de3ead3b0df834
Nemo Nakorn <75536335+Nemo-netidol@users.noreply.github.com>
2023-04-21 19:16:42 +0700

    Update KMeans.py

--- KMeans/KMeans.py
+++ KMeans/KMeans.py
@@ -12,7 +12,7 @@ y_pred = model.predict(x)
 new_y_pred = model.predict(new_x)
 
 plt.scatter(x[:, 0], x[:, 1], c=y_pred)
-plt.scatter(new_x[:, 0], new_x[:, 1], c='orange', s=120) # Sample
+plt.scatter(new_x[:, 0], new_x[:, 1], c='orange', s=120) # Samples
 plt.scatter(center[0, 0], center[0, 1], c='blue', label='Centeroid 1')
 plt.scatter(center[1, 0], center[1, 1], c='green', label='Centeroid 2')
 plt.scatter(center[2, 0], center[2, 1], c='red', label='Centeroid 3')
